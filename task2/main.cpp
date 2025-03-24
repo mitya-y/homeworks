c0bab996627dcdf5c9f5449f9c0a88dc553d840e
Dmitriy Kozenko <8863314@gmail.com>
2025-03-24 15:45:36 +0300

    added correct_c function for task2

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,6 +1,7 @@
 #include <print>
 #include <thread>
 #include <vector>
+#include <ranges>
 
 using namespace std::chrono_literals;
 
@@ -20,7 +21,7 @@ static void print_vec(const std::vector<uint64_t> &v) {
 }
 
 static void incorrect() {
-  uint32_t n = 100;
+  constexpr uint32_t n = 20;
   auto nums = get_rand_array(n);
 
   uint32_t thread_num = 5;
@@ -42,7 +43,7 @@ static void incorrect() {
 }
 
 static void correct_a() {
-  uint32_t n = 100;
+  constexpr uint32_t n = 100;
   auto nums = get_rand_array(n);
 
   uint32_t thread_num = 5;
@@ -60,11 +61,50 @@ static void correct_a() {
         auto sum = nums[i] + nums[j] + nums[k];
         nums[i] = nums[j] = nums[k] = sum;
       }
-    }
-    ));
+    }));
   }
 
   std::this_thread::sleep_for(1s);
+  // print_vec(nums);
+}
+
+static void correct_b() {
+  constexpr uint32_t n = 100;
+  auto nums = get_rand_array(n);
+
+  uint32_t thread_num = 5;
+  std::vector<std::jthread> threads;
+  threads.reserve(thread_num);
+
+  std::vector<std::mutex> mutexes(n);
+
+  for (uint32_t i = 0; i < thread_num; i++) {
+    threads.emplace_back(std::jthread([&](std::stop_token stop_token) {
+      while (!stop_token.stop_requested()) {
+        std::size_t i = rand() % n, j = rand() % n, k = rand() % n;
+        std::array idx {i, j, k};
+
+        uint32_t locked_n = 0;
+        for (auto l : idx) {
+          if (!mutexes[l].try_lock()) {
+            break;
+          }
+          locked_n++;
+        }
+
+        if (locked_n == idx.size()) {
+          auto sum = nums[i] + nums[j] + nums[k];
+          nums[i] = nums[j] = nums[k] = sum;
+        }
+
+        for (auto l : std::ranges::iota_view(0u, locked_n)) {
+          mutexes[l].unlock();
+        }
+      }
+    }));
+  }
+
+  std::this_thread::sleep_for(10ns);
   // print_vec(nums);
 }
 
@@ -86,18 +126,18 @@ static void correct_d() {
         std::sort(idx.begin(), idx.end());
 
         for (auto l : idx) {
-            mutexes[l].lock();
+          mutexes[l].lock();
         }
 
         auto sum = nums[i] + nums[j] + nums[k];
         nums[i] = nums[j] = nums[k] = sum;
 
-        for (auto l : idx) {
-            mutexes[l].unlock();
+        for (auto l : std::ranges::reverse_view(idx)) {
+        // for (auto l : idx) {
+          mutexes[l].unlock();
         }
       }
-    }
-    ));
+    }));
   }
 
   std::this_thread::sleep_for(1s);
@@ -106,6 +146,8 @@ static void correct_d() {
 
 
 int main() {
-  // incorrect();
+  incorrect();
   correct_a();
+  correct_b();
+  // correct_d();
 }
