46f67a95b997a4f1fde415dc324d949a6c025c94
Dmitriy Kozenko <8863314@gmail.com>
2025-05-04 13:32:29 +0300

    unlimited with condition variable work

--- logger/loggers/con_var_unlimited.hpp
+++ logger/loggers/con_var_unlimited.hpp
@@ -5,13 +5,14 @@
 #include "base.hpp"
 #include "msg.hpp"
 
-class CondVarLimitedLogger : public Logger {
+class CondVarUnlimitedLogger : public Logger {
 private:
   class UnlimitedQueue {
   private:
     std::queue<Message> queue;
     std::mutex mutex;
     std::condition_variable cv;
+    std::atomic_bool done = false;
 
   public:
     UnlimitedQueue() = default;
@@ -23,43 +24,41 @@ private:
       cv.notify_one();
     }
 
-    bool pop(Message &msg, std::atomic_bool &done) {
+    bool pop(Message &msg) {
       std::unique_lock lock(mutex);
-      cv.wait(lock, [this, &done]() {
+      cv.wait(lock, [this]() {
         return !queue.empty() || done;
       });
 
-      if (queue.empty()) {
+      if (queue.empty() || done) {
         return false;
       }
       msg = std::move(queue.front());
       queue.pop();
       return true;
     }
+
+    ~UnlimitedQueue() {
+      done = true;
+      cv.notify_one();
+    }
   };
   UnlimitedQueue queue;
-  std::atomic_bool done = false;
 
 public:
-  CondVarLimitedLogger(std::ostream &out) : Logger(out) {
+  CondVarUnlimitedLogger(std::ostream &out) : Logger(out) {
     log_thread = std::jthread([&](std::stop_token stop_token) {
       while (!stop_token.stop_requested()) {
         Message msg;
-        if (queue.pop(msg, done)) {
+        if (queue.pop(msg)) {
           std::lock_guard lock(log_mutex);
           out << msg.data;
         }
       }
-      std::println("leave from logger therad");
     });
   }
 
   void log(std::string_view msg) override {
     queue.push(Message(msg));
-  }
-
-  ~CondVarLimitedLogger() {
-    done = true;
-    std::println("logger desturct");
   }
 };
