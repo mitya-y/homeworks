c6e4a170972319322eff211f60a7af18445e2991
Dmitriy Kozenko <8863314@gmail.com>
2025-05-10 09:58:27 +0300

    imporoved tests

--- logger/main.cpp
+++ logger/main.cpp
@@ -10,7 +10,7 @@
 using namespace std::chrono_literals;
 
 template<typename LoggerT>
-static void test_cond_var_unlimited(bool random_delay = false) {
+static void test_logger(bool random_delay = false) {
   std::ofstream out(std::format("{}-rand_del-{}.txt", typeid(LoggerT).name(), random_delay));
   LoggerT logger(out);
 
@@ -19,22 +19,36 @@ static void test_cond_var_unlimited(bool random_delay = false) {
   threads.reserve(n);
 
   for (uint32_t th = 0; th < n; th++) {
-    threads.emplace_back([&logger, th](std::stop_token stop_token) {
+    threads.emplace_back([&logger, th, random_delay](std::stop_token stop_token) {
       while (!stop_token.stop_requested()) {
         auto msg = std::format("hello from {} thread\n", th);
         logger.log(msg);
-        std::this_thread::sleep_for(10ns);
-        // std::chrono::nanoseconds(10);
+        auto delay = std::chrono::nanoseconds(random_delay ? (rand() % 100) : 10);
+        std::this_thread::sleep_for(delay);
       }
     });
   }
 
   std::this_thread::sleep_for(100ms);
 
-  std::println("{} done", typeid(LoggerT).name());
+  std::println("{} with{} random delay done", typeid(LoggerT).name(), random_delay ? "" : "out");
+}
+
+using Loggers = std::tuple<
+  CondVarUnlimitedLogger,
+  CondVarLimitedLogger
+>;
+
+template<std::size_t index = 0>
+void test() {
+  if constexpr (index < std::tuple_size<Loggers>::value) {
+    using T = std::tuple_element_t<index, Loggers>;
+    test_logger<T>(false);
+    test_logger<T>(true);
+    test<index + 1>();
+  }
 }
 
 int main() {
-  test_cond_var_unlimited<CondVarUnlimitedLogger>();
-  test_cond_var_unlimited<CondVarLimitedLogger>();
+  test();
 }
